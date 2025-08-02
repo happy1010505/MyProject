e0cddb4b5066eeee265a8b86538e2e89ef9a275b
harry.lin <harry.lin@kli.com.tw>
2025-08-02 21:08:44 +0800

    帳號功能 - 使用MD5 加密密碼

--- src/main/java/com/harry/myproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/harry/myproject/service/impl/UserServiceImpl.java
@@ -5,11 +5,13 @@ import com.harry.myproject.dto.UserLoginRequest;
 import com.harry.myproject.dto.UserRegisterRequest;
 import com.harry.myproject.model.User;
 import com.harry.myproject.service.UserService;
+import org.apache.tomcat.util.Diagnostics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -28,6 +30,10 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -40,11 +46,16 @@ public class UserServiceImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+        // 檢查 user 是否存在
         if(user == null){
             log.warn("該 email {} 尚未被註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
